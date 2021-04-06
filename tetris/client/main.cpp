a00a0e8564647ab970da88c20a47b4df90c5a6a4
Myounghun <hoonisone@gmail.com>
2021-04-06 19:33:54 +0900

    client imp

--- tetris/client/main.cpp
+++ tetris/client/main.cpp
@@ -1,18 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <WinSock2.h>
-#include "clientSocket.h"
+#include "client.h"
 
+#define SCENE_LOGIN 0
+#define COMMAND_SIZE 1024
 
+char ip[100] = "127.0.0.1";
+int port = 5000;
+char request[BUFFER_SIZE];
+int scene = SCENE_LOGIN;
+ClientSocket* cs;
 
 int main(int argc, char* argv[])
 {
-    char ip[100] = "127.0.0.1";
-    int port = 5000;
-    char buffer[BUFFER_SIZE];
-    ClientSocket* cs = clientSocketCreate(ip, port);
-    int len = clientSocketReceive(cs, buffer);
-    printf("Massage(%d): %s\n", len, buffer);
-    clientSocketSend(cs, (char*)"hello");
+    cs = clientSocketCreate(ip, port);
+
+    while (1) {
+        switch (scene) {
+        case SCENE_LOGIN:
+            char command[COMMAND_SIZE];
+            scanf("%s", command);
+
+        }
+        int len = clientSocketRequest(cs, (char*)"hello gg");
+        printf("Massage(%d): %s\n", len, request);
+    }
     clientSocketTerminate(cs);
 }
