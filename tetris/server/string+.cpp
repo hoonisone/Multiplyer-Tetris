6b0de704358c286dc55a81f7730ecd7977159bd3
Myounghun <hoonisone@gmail.com>
2021-04-07 10:53:50 +0900

    string+

---
+++ tetris/server/string+.cpp
@@ -0,0 +1,14 @@
+#pragma warning(disable:4996)
+#include <string.h>
+#include "string+.h"
+#include "list.h"
+
+List* split(char* string, char* key) {
+	List* list = listCreateList(listStringEqual, listStringCompare, listStringPrint);
+	char* ptr = strtok(string, key);
+	while (ptr != NULL) {
+		listPushBackElement(list, ptr);
+		ptr = strtok(NULL, key);
+	}
+	return list;
+}
---
+++ tetris/server/string+.h
@@ -0,0 +1,4 @@
+#pragma once
+#include "list.h"
+
+List* split(char* string, char* key);
