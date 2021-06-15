e6fb3ad5c0b7f73253d099849526ea331d3b3e2f
hoonisone <hoonisone@google.com>
2021-06-16 02:26:56 +0900

    mainBoloc

--- tetris_server/source/mainBlock.cpp
+++ tetris_server/source/mainBlock.cpp
@@ -2,22 +2,23 @@
 #include "error.h"
 
 static MainBlockFunction* mainBlockFunctionCreate();
-static MainBlocK* mainBlockCreate(BlockBoard* blockBoard, Block* firstBlock);
-static Block* mainBlockChangeMainBlock(MainBlocK* mainBlockManager, Block* block);
-static void mainBlockInitShadowBlock(MainBlocK* mainBlockManager);
-static void mainBlockUpdateShawdowBlock(MainBlocK* mainBlockManager);
-static void mainBlockUpdateShawdowBlockPosition(MainBlocK* mainBlockManager);
-static void mainBlockMoveUp(MainBlocK* mainBlockManager);
-static void mainBlockMoveDown(MainBlocK* mainBlockManager);
-static void mainBlockMoveRight(MainBlocK* mainBlockManager);
-static void mainBlockMoveLeft(MainBlocK* mainBlockManager);
-static int mainBlockMove(MainBlocK* mainBlockManager, void(*move)(Block* block), void(*back)(Block* block));
-static void mainBlockDrawMainBlock(MainBlocK* mainBlockManager);
-static void mainBlockEraseMainBlock(MainBlocK* mainBlockManager);
-static void mainBlockDrawShadowBlock(MainBlocK* mainBlockManager);
-static void mainBlockEraseShadowBlock(MainBlocK* mainBlockManager);
+static MainBlock* mainBlockCreate(BlockBoard* blockBoard, Block* firstBlock);
+static Block* mainBlockChangeMainBlock(MainBlock* mainBlockManager, Block* block);
+static void mainBlockInitShadowBlock(MainBlock* mainBlockManager);
+static void mainBlockUpdateShawdowBlock(MainBlock* mainBlockManager);
+static void mainBlockUpdateShawdowBlockPosition(MainBlock* mainBlockManager);
+static int mainBlockMoveUp(MainBlock* mainBlockManager);
+static int mainBlockMoveDown(MainBlock* mainBlockManager);
+static int mainBlockMoveRight(MainBlock* mainBlockManager);
+static int mainBlockMoveLeft(MainBlock* mainBlockManager);
+static void mainBlockMoveToOrigin(MainBlock* mainBlockManager);
+static int mainBlockMoveOneSpace(MainBlock* mainBlockManager, void(*move)(Block* block), void(*back)(Block* block));
+static void mainBlockDrawMainBlock(MainBlock* mainBlockManager);
+static void mainBlockEraseMainBlock(MainBlock* mainBlockManager);
+static void mainBlockDrawShadowBlock(MainBlock* mainBlockManager);
+static void mainBlockEraseShadowBlock(MainBlock* mainBlockManager);
 
-static MainBlockFunction* getMainBlockFunction() {
+MainBlockFunction* getMainBlockFunction() {
 	static MainBlockFunction* mainBlockManagerFunction = mainBlockFunctionCreate();
 	return mainBlockManagerFunction;
 }
@@ -31,24 +32,31 @@ static MainBlockFunction* mainBlockFunctionCreate() {
 	object->moveLeft = mainBlockMoveLeft;
 	return object;
 }
-static MainBlocK* mainBlockCreate(BlockBoard* blockBoard, Block* firstBlock) {
+static MainBlock* mainBlockCreate(BlockBoard* blockBoard, Block* firstBlock) {
+
 	if (blockBoard == NULL) {
 		errorPrint("blockBoard is NULL");
 	}
 	if (firstBlock == NULL) {
 		errorPrint("firstBlock is NULL");
 	}
-	MainBlocK* object = (MainBlocK*)malloc(sizeof(MainBlocK));
+	MainBlock* object = (MainBlock*)malloc(sizeof(MainBlock));
+	memset(object, 0, sizeof(MainBlock));
 	if (object == NULL) {
 		errorPrint("memory allocation failed ");
 	}
+
 	object->blockBoard = blockBoard;
-	mainBlockChangeMainBlock(object, firstBlock);
+
 	mainBlockInitShadowBlock(object);
+	mainBlockChangeMainBlock(object, firstBlock);
+	mainBlockDrawMainBlock(object);
+
+	mainBlockDrawShadowBlock(object);
 	return object;
 }
 
-static Block* mainBlockChangeMainBlock(MainBlocK* mainBlockManager, Block* block) {	// change main block into block and return origin main block
+static Block* mainBlockChangeMainBlock(MainBlock* mainBlockManager, Block* block) {	// change main block into block and return origin main block
 	if (mainBlockManager == NULL) {
 		errorPrint("mainBlockManager is NULL");
 	}
@@ -56,22 +64,29 @@ static Block* mainBlockChangeMainBlock(MainBlocK* mainBlockManager, Block* block
 		errorPrint("block is NULL");
 	}
 	Block* originBlock = mainBlockManager->mainBlock;
+
+	if(!originBlock == NULL)
+		mainBlockEraseMainBlock(mainBlockManager);
 	mainBlockManager->mainBlock = block;
+
+	mainBlockMoveToOrigin(mainBlockManager);
+	mainBlockDrawMainBlock(mainBlockManager);
 	mainBlockUpdateShawdowBlock(mainBlockManager);
 	return originBlock;
 }
-static void mainBlockInitShadowBlock(MainBlocK* mainBlockManager) {	// 한 번만 수행
-	Block* shadowBlock = BLOCK->create(0, 0, 0, 0);
-	mainBlockUpdateShawdowBlock(mainBlockManager);
+static void mainBlockInitShadowBlock(MainBlock* mainBlockManager) {	// 한 번만 수행
+	Block* shadowBlock = BLOCK->create(0, 0, 0, 0, 0);
+	GRAPHIC->changeColor(WHITE);
+	mainBlockManager->shadowBlock = shadowBlock;
 }
-static void mainBlockUpdateShawdowBlock(MainBlocK* mainBlockManager) {
+static void mainBlockUpdateShawdowBlock(MainBlock* mainBlockManager) {
 	Block* shadowBlock = mainBlockManager->shadowBlock;
 	Block* mainBlock = mainBlockManager->mainBlock;
 	memcpy(shadowBlock, mainBlock, sizeof(Block));
 	BLOCK->setLetter(shadowBlock, (char*)"□");
 	mainBlockUpdateShawdowBlockPosition(mainBlockManager);
 }
-static void mainBlockUpdateShawdowBlockPosition(MainBlocK* mainBlockManager) {
+static void mainBlockUpdateShawdowBlockPosition(MainBlock* mainBlockManager) {
 	BlockBoard* blockBoard = mainBlockManager->blockBoard;
 	Block* mainBlock = mainBlockManager->mainBlock;
 	Block* shadowBlock = mainBlockManager->shadowBlock;
@@ -85,53 +100,66 @@ static void mainBlockUpdateShawdowBlockPosition(MainBlocK* mainBlockManager) {
 	mainBlock->y = originY;
 }
 
-static void mainBlockMoveUp(MainBlocK* mainBlockManager) {
-	mainBlockMove(mainBlockManager, BLOCK->moveUp, BLOCK->moveDown);
+static int mainBlockMoveUp(MainBlock* mainBlockManager) {
+	return mainBlockMoveOneSpace(mainBlockManager, BLOCK->moveUp, BLOCK->moveDown);
 }
-static void mainBlockMoveDown(MainBlocK* mainBlockManager) {
-	mainBlockMove(mainBlockManager, BLOCK->moveDown, BLOCK->moveUp);
+static int mainBlockMoveDown(MainBlock* mainBlockManager) {
+	return mainBlockMoveOneSpace(mainBlockManager, BLOCK->moveDown, BLOCK->moveUp);
 }
-static void mainBlockMoveRight(MainBlocK* mainBlockManager) {
-	int success = mainBlockMove(mainBlockManager, BLOCK->moveRight, BLOCK->moveLeft);
+static int mainBlockMoveRight(MainBlock* mainBlockManager) {
+	int success = mainBlockMoveOneSpace(mainBlockManager, BLOCK->moveRight, BLOCK->moveLeft);
 	if (success) {// 좌우로 움질일 때만 shadowBlock갱신
-		mainBlockUpdateShawdowBlockPosition(mainBlockManager);	
+		mainBlockEraseShadowBlock(mainBlockManager);
+		mainBlockUpdateShawdowBlockPosition(mainBlockManager);
+		mainBlockDrawShadowBlock(mainBlockManager);
 	}
+	return success;
 }
-static void mainBlockMoveLeft(MainBlocK* mainBlockManager) {
-	int success = mainBlockMove(mainBlockManager, BLOCK->moveLeft, BLOCK->moveRight);
+static int mainBlockMoveLeft(MainBlock* mainBlockManager) {
+	int success = mainBlockMoveOneSpace(mainBlockManager, BLOCK->moveLeft, BLOCK->moveRight);
 	if (success) {// 좌우로 움질일 때만 shadowBlock갱신
+		mainBlockEraseShadowBlock(mainBlockManager);
 		mainBlockUpdateShawdowBlockPosition(mainBlockManager);	
+		mainBlockDrawShadowBlock(mainBlockManager);
 	}
+	return success;
 }
-
+static void mainBlockMoveToOrigin(MainBlock* mainBlockManager) {
+	BlockBoard* blockBoard = mainBlockManager->blockBoard;
+	Block* mainBlock = mainBlockManager->mainBlock;
+	int destX = blockBoard->x + blockBoard->width/2 - BLOCK_WIDTH/2;
+	int destY = blockBoard->y;
+	BLOCK->moveTo(mainBlock, destX, destY);
+}
 // mainBlock에 대해 move를 수행하고 범위를 벗어나면 back를 수행한다.
 // 이동 성공 여부 반환
-static int mainBlockMove(MainBlocK* mainBlockManager, void (*move)(Block* block), void (*back)(Block* block)) {
+static int mainBlockMoveOneSpace(MainBlock* mainBlockManager, void (*move)(Block* block), void (*back)(Block* block)) {
 	int flag = 1;
 	if (mainBlockManager == NULL) {
 		errorPrint("mainBlockManager is NULL");
 	}
 	BlockBoard* blockBoard = mainBlockManager->blockBoard;
 	Block* mainBlock = mainBlockManager->mainBlock;
-	BLOCK->del(mainBlock);
+	mainBlockEraseMainBlock(mainBlockManager);
 	move(mainBlock);
 	if (!BLOCK_BOARD->blockPositionPermitCheck(blockBoard, mainBlock)) {
 		back(mainBlock);
 		flag = 0;
 	}
-	BLOCK->drawBlock(mainBlock);
+	mainBlockDrawMainBlock(mainBlockManager);
 	return flag;
 }
 
-static void mainBlockDrawMainBlock(MainBlocK* mainBlockManager) {
+static void mainBlockDrawMainBlock(MainBlock* mainBlockManager) {
 	BLOCK->drawBlock(mainBlockManager->mainBlock);
 }
-static void mainBlockEraseMainBlock(MainBlocK* mainBlockManager) {
-	BLOCK->del(mainBlockManager->mainBlock);
+static void mainBlockEraseMainBlock(MainBlock* mainBlockManager) {
+	
+	BLOCK->erase(mainBlockManager->mainBlock);
 }
-static void mainBlockDrawShadowBlock(MainBlocK* mainBlockManager) {
+static void mainBlockDrawShadowBlock(MainBlock* mainBlockManager) {
 	BLOCK->drawBlock(mainBlockManager->shadowBlock);
 }
-static void mainBlockEraseShadowBlock(MainBlocK* mainBlockManager) {
-	BLOCK->del(mainBlockManager->shadowBlock);
+static void mainBlockEraseShadowBlock(MainBlock* mainBlockManager) {
+	BLOCK->erase(mainBlockManager->shadowBlock);
 }
