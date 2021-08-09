0d2febf2524794e6a95558c0596332b44fe4a515
Hoon1029 <hoonisone@gmail.com>
2021-08-09 09:44:26 +0900

    consol

--- tetris_server/source/Consol.cpp
+++ tetris_server/source/Consol.cpp
@@ -8,4 +8,4 @@ void Consol::changeBackgroundColor(Color color){
 			CONSOLE_SCREEN_BUFFER_INFO info;
 		GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &info);
 		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), ((color & 0xf) << 4) | (info.wAttributes & 0xf));
-}
+} 
